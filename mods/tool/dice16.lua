636d7095f91923eddcbc8123ddd069dc2b2d4896
Shane McDonagh <flowingpoint5@hotmail.com>
2026-02-26 07:59:45 +0000

    dicebox
    mysterious scoring mechanism left as a puzzle to solve.

--- mods/tool/dice16.lua
+++ mods/tool/dice16.lua
@@ -1,8 +1,15 @@
 tool={}
-minetest.register_node("tool:dice_box", {
+
+minetest.register_node("tool:dice_box_covered", {
     description = "The Impossible Dice Box",
     drawtype = "nodebox",
-    tiles = {"wood01.png"},
+    paramtype2 = "facedir",
+    tiles = {"wood00.png^kopper_dbcover.png^[transformR180",
+    	"wood00.png^kopper_db.png",
+	"wood00.png^kopper_db.png",
+	"wood00.png^kopper_db.png",
+	"wood00.png^kopper_db.png",
+	"wood00.png^kopper_db.png"},
     paramtype = "light",
     use_texture_alpha = "clip",
     groups = {snappy = 3, attached_node = 1,not_in_creative_inventory=0},
@@ -12,15 +19,61 @@ minetest.register_node("tool:dice_box", {
             {-0.5, -0.5, -0.5, 0.5, -0.4, 0.5}, -- Floor
             {-0.5, -0.4, -0.5, -0.4, 0.1, 0.5}, -- West Wall
             {0.4, -0.4, -0.5, 0.5, 0.1, 0.5},  -- East Wall
-            {-0.4, -0.4, -0.5, 0.4, 0.1, -0.4}, -- South Wall
-            {-0.4, -0.4, 0.4, 0.4, 0.1, 0.5},  -- North Wall
+            {-0.4, -0.4, -0.5, -0.0625, -0.1, -0.4}, -- South Wall A
+            {0.0625, -0.4, -0.5, 0.4, -0.1, -0.4}, --South Wall B
+            {-0.0625, -0.4, -0.5, 0.0625, -0.1875, -0.4}, --South opening
+            {-0.4, -0.4, 0.4, 0.4, 0.1, 0.5}, -- North Wall
+            {-0.4, -0.1, -0.5, -0.0625, 0.1, -0.4}, -- Cover A
+            {0.0625, -0.1, -0.5, 0.4, 0.1, 0.4}, --Cover B
+            {-0.0625, -0.0625, -0.5, 0.0625, 0.1, -0.4}, --Cover opening
+            {-0.5, 0.1, -0.5, 0.5, 0.1875, 0.5} --Cover
+        },
+    },
+    on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
+	minetest.set_node(pos, {name = "tool:dice_box", param2 = node.param2})
+        minetest.sound_play({name="dmf", gain=0.05}, {pos=selfpos}, true)
+	end,
+    on_rotate = function(pos, node, user, mode, new_param2)
+    	if mode == screwdriver.ROTATE_FACE then
+    		return false
+    	end
+    end
+})
+
+minetest.register_node("tool:dice_box", {
+    description = "The Impossible Dice Box",
+    drawtype = "nodebox",
+    paramtype2 = "facedir",
+    tiles = {"wood00.png^kopper_9.png",
+    	"wood00.png^kopper_db.png",
+	"wood00.png^kopper_db.png",
+	"wood00.png^kopper_db.png",
+	"wood00.png^kopper_dbdice.png",
+	"wood00.png^kopper_db.png"},
+    paramtype = "light",
+    use_texture_alpha = "clip",
+    groups = {snappy = 3, attached_node = 1,not_in_creative_inventory=1},
+    node_box = {
+        type = "fixed",
+        fixed = {
+            {-0.5, -0.5, -0.5, 0.5, -0.4, 0.5}, -- Floor
+            {-0.5, -0.4, -0.5, -0.4, 0.1, 0.5}, -- West Wall
+            {0.4, -0.4, -0.5, 0.5, 0.1, 0.5},  -- East Wall
+            {-0.4, -0.4, -0.5, -0.0625, -0.1, -0.4}, -- South Wall A
+            {0.0625, -0.4, -0.5, 0.4, -0.1, -0.4}, --South Wall B
+            {-0.0625, -0.4, -0.5, 0.0625, -0.1875, -0.4}, --South opening
+            {-0.4, -0.4, 0.4, 0.4, 0.1, 0.5}, -- North Wall
+            {-0.4, 1, 0.2, -0.0625, 1.1, 0.4}, -- Cover A
+            {0.0625, 1, 0.2, 0.4, 1.1, 0.4}, --Cover B
+            {-0.0625, 1, 0.25, 0.0625, 1.1, 0.4}, --Cover opening
+            {-0.5, 0.1, 0.4, 0.5, 1.1, 0.5} -- Cover On Hinge
         },
     },
 
     -- Helper function to clear dice in this box
     on_rightclick = function(pos, node, clicker)
         local meta = minetest.get_meta(pos)
-        
+        minetest.sound_play({name="dwf", gain=0.05}, {pos=selfpos}, true)
         -- 1. Reset Display and Clear old dice
         meta:set_string("infotext", "Analyzing alternate realities...")
         
@@ -34,8 +87,8 @@ minetest.register_node("tool:dice_box", {
 
         -- 2. Create and Shuffle 16 slots (4x4 grid)
         local slots = {}
-        for x = 1, 4 do
-            for z = 1, 4 do
+        for x = 1, 3 do
+            for z = 1, 3 do
                 table.insert(slots, {x = x, z = z})
             end
         end
@@ -47,15 +100,15 @@ minetest.register_node("tool:dice_box", {
 
         -- 3. Spawn 3 Dice
         local total_score = 0
-        for i = 1, 3 do
+        for i = 1, 4 do
             local pick = slots[i]
             
             -- This is the missing 'spawn_pos' logic:
             -- Maps grid 1-4 to local coordinates -0.3 to 0.3
             local spawn_pos = {
-                x = pos.x + (pick.x * 0.2) - 0.5,
+                x = pos.x + (pick.x * 0.25) - 0.5,
                 y = pos.y - 0.35,
-                z = pos.z + (pick.z * 0.2) - 0.5
+                z = pos.z + (pick.z * 0.25) - 0.5
             }
             
             local ent = minetest.add_entity(spawn_pos, "tool:impossible_die")
@@ -66,20 +119,10 @@ minetest.register_node("tool:dice_box", {
             end
         end
 
-        -- 4. The Delayed Reveal (only one instance needed!)
-        minetest.after(2, function()
-            local current_node = minetest.get_node(pos)
-            if current_node.name == "tool:dice_box" then
-                meta:set_string("infotext", "Consolidated Reality Score: " .. total_score)
-            end
-        end)
-    
-
-
-    -- 5. Set the hovertext timer on the box metadata
+    -- 4. Set the hovertext timer on the box metadata
     local meta = minetest.get_meta(pos)
-    meta:set_string("infotext", "Rolling...")
-    minetest.after(2, function()
+    meta:set_string("infotext", "...")
+    minetest.after(1, function()
         -- Only show if the box is still there
         local current_node = minetest.get_node(pos)
         if current_node.name == "tool:dice_box" then
@@ -98,54 +141,12 @@ end,
             end
         end
     end,
+        on_punch = function(pos, node, clicker, itemstack, pointed_thing)
+	minetest.set_node(pos, {name = "tool:dice_box_covered", param2 = node.param2})
+        minetest.sound_play({name="dmf", gain=0.05}, {pos=selfpos}, true)
+	end
 })
 
--- Inside your minetest.register_entity("tool:impossible_die", {...})
-
-roll = function(self)
-    -- 1. Pick 6 random textures from the pool of 16
-    local new_faces = get_random_six(glyph_pool)
-    self.object:set_properties({ textures = new_faces })
-
-    -- 2. Randomize horizontal "kick" (velocity)
-    -- Enough to hit the walls but not fly out
-local vx = (math.random() - 0.5) * 2.0 -- Speed between -1 and 1
-local vz = (math.random() - 0.5) * 2.0
-local vy = 0.5 + math.random() -- Random jump height
-
-self.object:set_velocity({x=vx, y=vy, z=vz})
-    
-    -- 3. Gravity to pull them back down
-    self.object:set_acceleration({x=0, y=-9.81, z=0})
-
-    -- 4. Add a random rotation spin for visual flair
-    self.object:set_rotation({
-        x = math.random() * math.pi * 2,
-        y = math.random() * math.pi * 2,
-        z = math.random() * math.pi * 2
-    })
-
-
-end
-
-on_step = function(self, dtime)
-    local vel = self.object:get_velocity()
-    
-    -- If the die is moving (falling or being punched), make it spin
-    if math.abs(vel.y) > 0.1 or math.abs(vel.x) > 0.1 then
-        local rot = self.object:get_rotation()
-        self.object:set_rotation({
-            x = rot.x + (math.pi * dtime * 2), -- 1 full rotation per second
-            y = rot.y + (math.pi * dtime * 1.5),
-            z = rot.z + (math.pi * dtime * 0.5)
-        })
-    else
-        -- When it stops, snap it to the nearest face (your existing snapping logic)
-        local rot = self.object:get_rotation()
-        local snap = function(r) return math.floor((r / (math.pi/2)) + 0.5) * (math.pi/2) end
-        self.object:set_rotation({x=snap(rot.x), y=snap(rot.y), z=snap(rot.z)})
-    end
-end
 
 tool={}
 local die_pool = {
@@ -176,9 +177,9 @@ end
 
 minetest.register_entity("tool:impossible_die", {
     visual = "cube",
-    visual_size = {x=0.1,y=0.1,z=0.1},
+    visual_size = {x=0.05,y=0.05,z=0.05},
     textures = {"wood00.png^dice_1.png", "wood00.png^dice_2.png", "wood00.png^dice_3.png", "wood00.png^dice_4.png", "wood00.png^dice_5.png", "wood00.png^dice_6.png"},
-    collisionbox = {-0.05, -0.05, -0.05, 0.05, 0.05, 0.05},
+    collisionbox = {-0.025, -0.025, -0.025, 0.025, 0.025, 0.025},
     physical = true,
     collide_with_objects = true,
     on_activate = function(self, staticdata)
@@ -205,7 +206,7 @@ roll_instant = function(self)
         table.insert(tex_table, "wood00.png^" .. filename)
     end
 
-    -- 3. Instant Snap Rotation
+--     3. Instant Snap Rotation
     local r_list = {0, math.pi/2, math.pi, math.pi*1.5}
     self.object:set_properties({
         textures = tex_table,
@@ -216,7 +217,7 @@ roll_instant = function(self)
         y = r_list[math.random(4)],
         z = r_list[math.random(4)]
     })
-
+    
     -- 4. Calculate the 'Reality Score'
     -- Pick which face is on top, then look up its score in the list
     local winning_subset_index = math.random(6)
